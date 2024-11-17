770f2a1ece4165d7fbb428b94d1f116824be7d97
adama-wzr <andrefadam@gmail.com>
2024-11-17 16:29:50 -0600

    2D CPU Parallel
    - 2D particle size distribution with parallel computing (openMP).
    - The 2D algorithm PSD works really well, significant speedup with parallel computing.

--- CPU/ED_PSD_CPU.cpp
+++ CPU/ED_PSD_CPU.cpp
@@ -11,3 +11,30 @@
 */
 
 #include "ED_PSD_CPU.hpp"
+
+int main(void){
+    // flags to determine which code to run
+    bool input2D;
+    bool debugMode, particleFlag, poreFlag;
+
+    input2D = true;
+    debugMode = true;
+    particleFlag = true;
+    poreFlag = false;
+
+    fflush(stdout);
+
+    if (input2D)
+    {
+        // 2D code
+        if (particleFlag)   ParticleSizeDist2D(debugMode);
+        if (poreFlag)       PoreSizeDist2D(debugMode);
+    } else
+    {
+        // 3D code
+        if (particleFlag)   ParticleSizeDist3D(debugMode);
+        if (poreFlag)       PoreSizeDist3D(debugMode);
+    }
+
+    return 0;
+}
